d523a17a8f83991380c2285d65ad595d25428d8e
Kevin Carbaugh <kcarbaug@mines.edu>
2014-04-11 08:34:57 -0600

    Added header

--- nim.f95
+++ nim.f95
@@ -1,1 +1,19 @@
+!---------------------------------------------------------------------------
+!  Header section:
+!  TITLE: CSCI260 Final Project - Nim Game
+!  AUTHORS: Kevin Carbaugh, Ryan Shipp
+!  CLASS: CSCI260A
+!  DATE WRITTEN: 4/11/14
+!  LAST REVISION: 4/11/14
+!  DESCRIPTION: This program allows users to play the game 'Nim'
+!               either against an AI or another human
+!
+!  DATA DICTIONAY
+!--------------------------------------------------------------------------
+!  VARIABLES USED:
+!       NAME:           TYPE:           COMMENT:
+!
+!
+!
+!---------------------------------------------------------------------------
 
