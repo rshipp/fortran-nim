9aa7ecdb5955697bcc1072f86ce2689b4a4a5486
george <rpubaddr0@gmail.com>
2014-04-16 10:47:56 -0600

    Working on dumb_player.

--- dumb_player.f95
+++ dumb_player.f95
@@ -1,29 +1,16 @@
-!---------------------------------------------------------------------------
-!  Header section:
-!  TITLE: CSCI260 Final Project - Nim Game
-!  AUTHORS: Kevin Carbaugh, Ryan Shipp
-!  CLASS: CSCI260A
-!  DATE WRITTEN: 4/11/14
-!  LAST REVISION: 4/11/14
-!  DESCRIPTION: This program allows users to play the game 'Nim'
-!               either against an AI or another human
-!
-!  DATA DICTIONAY
-!--------------------------------------------------------------------------
-!  VARIABLES USED:
-!       NAME:           TYPE:           COMMENT:
-!
-!
-!
-!---------------------------------------------------------------------------
+! Dumb computer player.
+! Makes random, valid moves.
 
 MODULE dumb_player
 IMPLICIT NONE
 CONTAINS
-    FUNCTION dumb_player_turn(board)
 
+FUNCTION dumb_player_turn(board, length)
+    INTEGER :: length
+    INTEGER, DIMENSION(length) :: board, dumb_player_turn
 
-        dumb_player_turn = board
-        RETURN
-    END FUNCTION dumb_player_turn
+    dumb_player_turn = board
+    RETURN
+END FUNCTION dumb_player_turn
+
 END MODULE dumb_player
