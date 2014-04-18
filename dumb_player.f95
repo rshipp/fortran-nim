35193e122723326667777e9ba1c0964c2b649bed
george <rpubaddr0@gmail.com>
2014-04-18 08:23:04 -0600

    Added logic for dumb computer turn.

--- dumb_player.f95
+++ dumb_player.f95
@@ -7,7 +7,26 @@ CONTAINS
 
 FUNCTION dumb_player_turn(board, length)
     INTEGER :: length
+    REAL    :: x, y
     INTEGER, DIMENSION(length) :: board, dumb_player_turn
+
+    ! Generate two random numbers.
+    CALL random_seed()
+    CALL RANDOM_NUMBER(x)
+    CALL RANDOM_NUMBER(y)
+
+    ! Normalize one agains the board length to get the row.
+    ! Make sure the row has >0 sticks left.
+    DO
+        row = 1 + (INT(x * length)
+	IF (board(row) .NE. 0) EXIT
+    END DO
+
+    ! Normalize the other against the row width to get the number of sticks.
+    sticks = 1 + INT(y * board(row))
+
+    ! Remove some sticks from a row.
+    board(row) = board(row) - sticks
 
     dumb_player_turn = board
     RETURN
