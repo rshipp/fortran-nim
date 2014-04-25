4ac6270a8ae01d890f76c7efa2155293f47a2886
george <rpubaddr0@gmail.com>
2014-04-25 08:20:09 -0600

    All done.

--- nim.f95
+++ nim.f95
@@ -26,24 +26,66 @@ INCLUDE "board_reader.f95"
 PROGRAM NIM
     USE PRINTBOARD
     USE READBOARD
+    USE dumb_player
+    USE human_player
     IMPLICIT NONE
-    INTEGER:: menu_choice, board_dim, err
+    INTEGER:: menu_choice, board_dim, err, turn
     INTEGER,DIMENSION(:),allocatable:: board
     10 WRITE(*,*) "Welcome to the game of nim. Please select an option:"
     WRITE(*,*) "1 - Play vs another human"
     WRITE(*,*) "2 - Play vs an AI"
     WRITE(*,*) "3 - How to play"
     READ(*,*) menu_choice
+    turn = 0
 
     SELECT CASE(menu_choice)
         CASE(1)
             CALL READ_BOARD(board, board_dim)
             CALL PRINT_BOARD(board, board_dim)
-            !TODO
+            DO
+                IF (MOD(turn, 2) .EQ. 0) THEN
+                    WRITE(*,*) ""
+                    WRITE(*,*) "Player 1's turn:"
+                    board = human_player_turn(board, board_dim)
+                    CALL PRINT_BOARD(board, board_dim)
+                    IF (game_won(board, board_dim)) THEN
+                        WRITE(*,*) "Player 2 won."
+                        STOP
+                    END IF
+                ELSE
+                    WRITE(*,*) ""
+                    WRITE(*,*) "Player 2's turn:"
+                    board = human_player_turn(board, board_dim)
+                    CALL PRINT_BOARD(board, board_dim)
+                    IF (game_won(board, board_dim)) THEN
+                        WRITE(*,*) "Player 1 won."
+                        STOP
+                    END IF
+                END IF
+                turn = turn + 1
+            END DO 
         CASE(2)
             CALL READ_BOARD(board, board_dim)
             CALL PRINT_BOARD(board, board_dim)
-            !TODO
+            DO
+                IF (MOD(turn, 2) .EQ. 0) THEN
+                    board = human_player_turn(board, board_dim)
+                    CALL PRINT_BOARD(board, board_dim)
+                    IF (game_won(board, board_dim)) THEN
+                        WRITE(*,*) "Computer player won."
+                        STOP
+                    END IF
+                ELSE
+                    WRITE(*,*) "Computer player's turn:"
+                    board = dumb_player_turn(board, board_dim)
+                    CALL PRINT_BOARD(board, board_dim)
+                    IF (game_won(board, board_dim)) THEN
+                        WRITE(*,*) "Human player won."
+                        STOP
+                    END IF
+                END IF
+                turn = turn + 1
+            END DO
         CASE(3)
             WRITE(*,*) "Nim is a game of strategy in which two players take turns picking up objects."
             WRITE(*,*) "In our case, these are represented by pipe characters (|). On their turn, a"
